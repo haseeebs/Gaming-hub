86126f1ac1d5fbe1ae44a44ed223de556b7b176f
haseeb hummo <haseebpersonalwork@gmail.com>
2024-07-12 04:56:57 +0530

    feat: build color mode switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/Gemini_Generated_Image.png'
+import ColorModeSwitch from './ColorModeSwitch'
 
 const Navbar = () => {
   return (
     <HStack>
         <Image src={logo} boxSize={'100px'}/>
-        <Text>Gaming console</Text>
+        <ColorModeSwitch />
     </HStack>
   )
 }

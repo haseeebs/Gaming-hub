c9e699db7074bdca8e8ec3f3ef3c235b3ddaf733
haseeb hummo <haseebpersonalwork@gmail.com>
2024-07-13 16:29:53 +0530

    feat: Add GenreList component and custom hook for fetching genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import apiClients from "../services/api-clients";
+import { CanceledError } from "axios";
+
+interface Genres {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresResponse {
+  count: number;
+  results: Genres[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genres[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClients
+      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+        setLoading(false);
+      });
+
+      return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;

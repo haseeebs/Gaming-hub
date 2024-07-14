30407c064c646aaa600b5a22d0cb500c0b8f556c
haseeb hummo <haseebpersonalwork@gmail.com>
2024-07-14 07:27:59 +0530

    feat: Add loading skeleton for GenreList

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,17 @@
 import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if(error) return null;
 
   return (
     <List>
+
+    {isLoading && <GenreListSkeleton />}
+
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY={'5px'}>
           <HStack>
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,20 @@
+import { Skeleton, Stack } from "@chakra-ui/react";
+
+const GenreListSkeleton = () => {
+  return (
+    <Stack>
+      <Skeleton height="40px" />
+      <Skeleton height="40px" />
+      <Skeleton height="40px" />
+      <Skeleton height="40px" />
+      <Skeleton height="40px" />
+      <Skeleton height="40px" />
+      <Skeleton height="40px" />
+      <Skeleton height="40px" />
+      <Skeleton height="40px" />
+      <Skeleton height="40px" />
+    </Stack>
+  );
+};
+
+export default GenreListSkeleton;

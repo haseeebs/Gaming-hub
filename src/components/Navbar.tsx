340d01b960232a47f0079c32d8fac44e295b96f9
haseeb hummo <haseebpersonalwork@gmail.com>
2024-07-16 08:13:47 +0530

    feat: implement searching

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,20 @@
-import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/Gemini_Generated_Image.png'
-import ColorModeSwitch from './ColorModeSwitch'
-import SearchInput from './SearchInput'
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/Gemini_Generated_Image.png";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
-const Navbar = () => {
-  return (
-    <HStack padding={'15px'}>
-        <Image src={logo} boxSize={'100px'}/>
-        <SearchInput />
-        <ColorModeSwitch />
-    </HStack>
-  )
+interface Props {
+  onSearch: (searchText: string) => void;
 }
 
-export default Navbar
+const Navbar = ({ onSearch }: Props) => {
+  return (
+    <HStack padding={"15px"}>
+      <Image src={logo} boxSize={"100px"} />
+      <SearchInput onSearch={onSearch} />
+      <ColorModeSwitch />
+    </HStack>
+  );
+};
+
+export default Navbar;
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,26 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { FormEvent, useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+    onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({onSearch}: Props) => {
+    const ref = useRef<HTMLInputElement>(null);
+  
+    const handleSubmit = (event: FormEvent) => {
+      event.preventDefault();
+      if (ref.current) onSearch(ref.current.value);
+    };
+  
   return (
-    <InputGroup>
-      <Input borderRadius={20} placeholder="Search games" variant={"filled"}/>
-      <InputLeftElement children={<BsSearch />} />
-    </InputGroup>
+    <form onSubmit={handleSubmit}>
+        <InputGroup>
+            <Input ref={ref} borderRadius={20} placeholder="Search games" variant={"filled"}/>
+            <InputLeftElement children={<BsSearch />} />
+        </InputGroup>
+    </form>
   );
 };
 
